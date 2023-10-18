702ceed86377e2fadf913019b91925580a8b3e83
shin-dongheon <tlsejdgjs@naver.com>
2023-10-18 15:02:25 +0900

    # 자바스크립트 객체

--- javascrpt-paractices/basics/es06.js
+++ javascrpt-paractices/basics/es06.js
@@ -11,9 +11,46 @@
 // 1. Number, Object, String, ... 내장 객체(내장 생성자 함수)를 사용하는 방법
 // 2. 사용자 정의 생성자 함수를 사용하는 방법
 
+var o1 = new Object();
+o1.name = '둘리';
+o1.age = 10;
+o1.another = new Object();
+o1.another.name = ' 마이콜';
+o1.another.age = 30;
+
+console.log(o1);
+
 
 // 생성 방법2
 // {} literal를 사용하는 방법
+var o2 = {};
+
+o2.name = '둘리';
+o2.age = 10;
+o2.another = new Object();
+o2.another.name = ' 마이콜';
+o2.another.age = 30;
+
+console.log(o2);
 
 // 생성 방법3
 // (J)ava(S)cript (O)bject (N)otation
+
+let o3 = {
+    name: '둘리',
+    age: 10,
+    another: {
+        name: '마이콜',
+        age: 30
+    }
+};
+
+console.log(o3);
+
+// XmlHttpReques, fetch
+// const response = fetch("/mysite07/api/user/1",{ })
+response = '{"no":1, "age":10, "name":"둘리", "email":"dooly@gamil.com"}';
+var user = JSON.parse(response);
+console.log(user.name, user.email);
+
+
