22de1075a73fe2d81e25601658c52401e4b5c6a1
shin-dongheon <tlsejdgjs@naver.com>
2023-10-19 16:02:15 +0900

    # dom api #preview

--- javascrpt-paractices/basics/es13.js
+++ javascrpt-paractices/basics/es13.js
@@ -37,3 +37,24 @@ console.log(a);
 
 a.insert(2, ['a', 'b', 'c']);
 console.log(a);
+
+
+// 4. encodeURIComponent 사용 예
+// 만들어야 하는 url : "http://www.mysite.com/user?name=둘리&email=dooly@gmail.com"
+
+var url = "http://www.mysite.com/user";
+var formData = {
+    name : "둘리",
+    email: "dooly@gmail.com"
+}
+
+var toQueryString = function(o){
+    var qs = [];
+    for(let prop in o){
+        qs.push(ptop + "=" + encodeURIComponent(o[prop]));
+    }
+    return qs.join("&");
+}
+
+console.log(url+"?"+toQueryString(formData));
+console.log(`${url}?${toQueryString(formData)}`);
---
+++ javascrpt-paractices/basics/exinfo.js
@@ -0,0 +1,6 @@
+
+// pop 배열 끝 요소
+let fruits = ["사과", "오렌지", "배"];
+
+console.log(fruits.pop());
+console.log(fruits);
